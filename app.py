7f1ab4c5c37e79fe6da62597ffc33673f07d2b3b
ycnekkkkkk <ycao123123@gmail.com>
2025-04-25 17:38:41 +0700

    Initial commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("in.html")
+    return render_template("in copy.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
